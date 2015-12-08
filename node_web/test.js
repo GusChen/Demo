fcf700d7daa94fe298fe21091083db1616ff13f6
guschen <guanxun_chen@foxmail.com>
2015-12-08 22:21:35 +0800

    rm test

--- node_web/test.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-readPath = path.join('view','index.html');
-var ext = path.extname(readPath);
-
-console.log("readPath:"+readPath);
-console.log("ext:"+ext);
-
-ext = ext.slice(1);
-console.log("ext:"+ext);
